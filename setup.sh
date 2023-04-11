4cdfc311081a463bd19455433b2055d100fdbf7c
Jim Hessin <jhessin@gmail.com>
2023-04-10 21:18:55 -0600

    Added a setup to download and install paq-nvim

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+git clone --depth=1 https://github.com/savq/paq-nvim.git \
+    "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/pack/paqs/start/paq-nvim
+
+nvim.appimage +PaqSync +q
+
