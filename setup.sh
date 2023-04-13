ceb4b4350371a933dea13c23cbf366afb3ec7ecb
Jim Hessin <jhessin@gmail.com>
2023-04-13 06:44:29 -0600

    updated setup for multiple runs

--- setup.sh
+++ setup.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
+#
+PAQ_DIRECTORY="${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/pack/paqs/start/paq-nvim
 
-git clone --depth=1 https://github.com/savq/paq-nvim.git \
-    "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/pack/paqs/start/paq-nvim
+if [[ ! -d "${PAQ_DIRECTORY}" ]]; then
+  echo "Downloading paq-nvim to ${PAQ_DIRECTORY}"
+  git clone --depth=1 https://github.com/savq/paq-nvim.git \
+    "${PAQ_DIRECTORY}"
+fi
 
-nvim.appimage +PaqSync +q
+nvim +PaqSync +q
 
